de974d98b1b918319d70c311e141fa43c8206b0e
Ziaul Kamal <codepad@usa.com>
2024-08-17 18:19:02 +0700

    update

--- src/app/api/google-trends/route.js
+++ src/app/api/google-trends/route.js
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server';
 import googleTrendsClient from '@/app/lib/googleTrendsClient';
 import { saveGoogleTrendsData } from '@/app/lib/supabase';
 
-let lastTimestamp = Math.floor(Date.now() / 1000);  // Default to current time if not set
 export const fetchCache = 'default-no-store';
 
 export async function GET(request) {
@@ -10,14 +9,6 @@ export async function GET(request) {
     const url = new URL(request.url);
     const geo = url.searchParams.get('geo') || 'ID';
     const timestamp = parseInt(url.searchParams.get('timestamp') || '0', 10);
-
-    // Cek apakah timestamp yang diterima lebih baru dari yang terakhir disimpan
-    if (timestamp <= lastTimestamp) {
-      return NextResponse.json({ status: 'info', message: 'No new data' });
-    }
-
-    // Simpan timestamp terbaru
-    lastTimestamp = timestamp;
 
     // Ambil data tren harian
     const trendsData = await googleTrendsClient.fetchDailyTrends(geo);
@@ -29,7 +20,7 @@ export async function GET(request) {
     // Simpan data tren harian ke Supabase
     await saveGoogleTrendsData(trendsData.default.trendingSearchesDays, geo);
 
-    return NextResponse.json({ status: 'success', message: 'Data fetched and saved successfully', newTimestamp: lastTimestamp });
+    return NextResponse.json({ status: 'success', message: 'Data fetched and saved successfully', newTimestamp: timestamp });
   } catch (error) {
     console.error('Error fetching and saving Google Trends data:', error.message);
     return NextResponse.json({ status: 'error', message: error.message }, { status: 500 });
