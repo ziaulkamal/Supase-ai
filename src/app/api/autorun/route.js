4ea3e5760885a88afc3aa8a6ae51c3657b464e26
Ziaul Kamal <codepad@usa.com>
2024-08-17 17:04:55 +0700

    update

--- src/app/api/autorun/route.js
+++ src/app/api/autorun/route.js
@@ -1,6 +1,7 @@
 // api/autorun/route.js
 
 import { NextResponse } from 'next/server';
+import axios from 'axios';
 
 export async function GET(request) {
   try {
@@ -8,22 +9,19 @@ export async function GET(request) {
     const timestamp = Math.floor(Date.now() / 1000);
 
     // Kirimkan request POST ke endpoint /api/google-trends dengan timestamp sebagai parameter
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/google-trends`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ geo: 'US', timestamp }) // Kirim parameter geo dan timestamp
-    });
+    const { data } = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/google-trends`,
+      { geo: 'US', timestamp },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
 
-    const result = await response.json();
-
-    if (result.status !== 'success') {
-      console.error('Error:', result.message);
-      return NextResponse.json({ status: 'error', message: result.message }, { status: 500 });
+    // Cek status hasil response
+    if (data.status !== 'success') {
+      console.error('Error:', data.message);
+      return NextResponse.json({ status: 'error', message: data.message }, { status: 500 });
     }
 
-    return NextResponse.json({ status: 'success', data: result.data });
+    return NextResponse.json({ status: 'success', data: data.data });
   } catch (error) {
     console.error('Error triggering Google Trends data fetch:', error.message);
     return NextResponse.json({ status: 'error', message: error.message }, { status: 500 });
