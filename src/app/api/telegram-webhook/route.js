78fc047bb5277f8a5ad2d364a3a3c084234d9893
Ziaul Kamal <codepad@usa.com>
2024-08-17 13:45:01 +0700

    update

--- src/app/api/telegram-webhook/route.js
+++ src/app/api/telegram-webhook/route.js
@@ -111,6 +111,14 @@ export async function POST(request) {
 
           await webHookTelegram({secretkey:text}, 'insertToken');
           await sendMessage(chatId, 'Berhasil menambahkan Token Baru ');
+        }else if(userState.state === 'set_count'){
+          const response = await webHookTelegram(null, 'read');
+          if (response) {
+            const { totalArticles, totalCategories } = response;
+            await sendMessage(chatId, `Jumlah Artikel: ${totalArticles}\nJumlah Kategori Unik: ${totalCategories}`);
+          } else {
+            await sendMessage(chatId, 'Terjadi kesalahan saat mengambil data konten.');
+          }
         }
       } else {
         await sendMessage(chatId, 'Untuk setiap aksi mulai dengan /run dan untuk mengakhiri sesi user /end');
