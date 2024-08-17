b995b58f159343b680b99fbf14cd5bf586d987ef
Ziaul Kamal <codepad@usa.com>
2024-08-17 17:25:00 +0700

    switch to redirect

--- src/app/api/autorun/route.js
+++ src/app/api/autorun/route.js
@@ -1,35 +1,21 @@
 // api/autorun/route.js
 
 import { NextResponse } from 'next/server';
-import axios from 'axios';
 
 export async function GET(request) {
   try {
     // Menghasilkan timestamp mirip dengan time() di PHP
     const timestamp = Math.floor(Date.now() / 1000);
 
-    // Kirimkan request GET ke endpoint /api/google-trends dengan geo dan timestamp sebagai parameter query string
-    const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/google-trends`, {
-      params: {
-        geo: 'ID',
-        timestamp
-      },
-      headers: { 
-        'Content-Type': 'application/json',
-        'Cache-Control': 'no-cache, no-store, must-revalidate', // Tambahkan header untuk mencegah cache
-        'Pragma': 'no-cache', // Untuk kompatibilitas dengan HTTP/1.0
-        'Expires': '0' // Mencegah caching pada server lama
-      }
-    });
+    // Buat URL tujuan untuk redirect
+    const url = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/api/google-trends`);
+    url.searchParams.append('geo', 'ID');
+    url.searchParams.append('timestamp', timestamp);
 
-    // Cek status hasil response
-    if (data.status !== 'success') {
-      console.error('Error:', data.message);
-      return NextResponse.json({ status: 'error', message: data.message }, { status: 500 });
-    }
+    // Redirect ke endpoint /api/google-trends dengan query string yang sesuai
+    const response = NextResponse.redirect(url);
 
-    // Kembalikan response dengan header Cache-Control yang sama
-    const response = NextResponse.json({ status: 'success', data: data.data });
+    // Tambahkan header untuk mencegah cache
     response.headers.set('Cache-Control', 'no-cache, no-store, must-revalidate');
     response.headers.set('Pragma', 'no-cache');
     response.headers.set('Expires', '0');
